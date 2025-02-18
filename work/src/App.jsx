a7f8b67422143de7c675b2ba393e5f409db4e06d
shamil <22bcais002@caias.in>
2025-02-18 18:03:52 +0530

    Remove unused route for login page in App component

--- work/src/App.jsx
+++ work/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LOGO/>} />
-        <Route path="/lo" element={<LOGO/>} />
 
       </Routes>
     </BrowserRouter>
